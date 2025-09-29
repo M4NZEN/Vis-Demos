14fbd9cf5c49877eb640a5360ba04fcbdfced4bc
M4NZEN <luzdevuelta.27man@gmail.com>
2025-09-28 21:25:11 -0500

    Demo dark

--- Demo/script.js
+++ Demo/script.js
@@ -3,7 +3,10 @@ const nodes = new vis.DataSet([
   { id: 2, label: "Nota 2" },
   { id: 3, label: "Nota 3" },
   { id: 4, label: "Nota 4" },
-  { id: 5, label: "Nota 5" }
+  { id: 5, label: "Nota 5" },
+  { id: 6, label: "Nota 6" },
+  { id: 7, label: "Nota 7" },
+  { id: 8, label: "Nota 8" }
 ]);
 
 const edges = new vis.DataSet([
@@ -11,7 +14,7 @@ const edges = new vis.DataSet([
   { from: 1, to: 2 },
   { from: 2, to: 4 },
   { from: 2, to: 5 },
-  { from: 3, to: 3 }
+  { from: 3, to: 2 }
 ]);
 
 const container = document.getElementById("mynetwork");
@@ -22,14 +25,19 @@ const options = {
     shape: "dot",
     size: 20,
     color: "#007bff",
-    font: { color: "#000" }
+    font: { color: "#f1ebebff" }
   },
   edges: {
     color: { color: "#aaa" },
     arrows: "to"
   },
   physics: {
-    stabilization: false
+    stabilization: false, 
+    barnesHut: {
+      gravitationalConstant: -8000,
+    },
+    minVelocity: 0.1,
+    solver: 'barnesHut'
   }
 };
 
